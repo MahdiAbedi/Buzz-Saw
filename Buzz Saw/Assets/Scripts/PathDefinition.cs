a8972a2cb69174e5cef2d6de656ca5fa532755f2
Mahdi Abedi <mahdiabedi220@yahoo.com>
2017-12-12 16:42:34 +0330

    step2
    drow line between points

---
+++ Buzz Saw/Assets/Scripts/PathDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDefinition : MonoBehaviour {
+
+    public Transform[] Points;
+
+
+
+    void OnDrawGizmos()
+    {
+        if (Points==null || Points.Length<2)
+        {
+            return;
+        }
+
+        for (int i = 1; i < Points.Length; i++)
+        {
+            Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+        }
+    }
+}
