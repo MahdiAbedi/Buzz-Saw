ab9c75a70ad63926652f28982f77b1cdc088eddb
Mahdi Abedi <mahdiabedi220@yahoo.com>
2017-12-16 16:52:56 +0330

    pathPlatformCodes
    path find
    pathPlatform
    codes are created and tested

---
+++ Buzz Saw/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+    public enum FollowType
+    {
+        MoveTowards,
+        lerp
+    }
+
+    public FollowType Type = FollowType.MoveTowards;
+    public PathDefinition Path;
+    public float Speed = 1f;
+    public float MaxDistanceToGoal = 0.1f;
+
+    private IEnumerator<Transform> _currentPoint;
+
+	// Use this for initialization
+	void Start () {
+        if (Path==null)
+        {
+            Debug.LogError("path cannot be null",gameObject);
+            return;
+        }
+
+        _currentPoint = Path.GetPathEnumarator();
+        _currentPoint.MoveNext();
+
+        if (_currentPoint.Current == null) return;
+
+        transform.position = _currentPoint.Current.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_currentPoint == null || _currentPoint.Current == null) return;
+
+        if (Type==FollowType.MoveTowards)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,_currentPoint.Current.position,Time.deltaTime*Speed);
+        }
+        else if (Type==FollowType.lerp)
+        {
+            transform.position = Vector3.Lerp(transform.position, _currentPoint.Current.position, Time.deltaTime * Speed);
+        }
+
+        var distanceSquared = (transform.position-_currentPoint.Current.position).sqrMagnitude;
+        if (distanceSquared<MaxDistanceToGoal*MaxDistanceToGoal)
+        {
+            _currentPoint.MoveNext();
+        }
+	}
+}
--- Buzz Saw/Assets/Scripts/PathDefinition.cs
+++ Buzz Saw/Assets/Scripts/PathDefinition.cs
@@ -5,8 +5,29 @@ using UnityEngine;
 public class PathDefinition : MonoBehaviour {
 
     public Transform[] Points;
+    //در واقع تو این تابع ما داریم تو هر مرحله نقطه حرکت بعدی رو مشخص میکنیم
+    public IEnumerator<Transform> GetPathEnumarator()
+    {
+        if (Points==null|| Points.Length<=1)
+        {
+            yield break;
+        }
 
+        var direction = 1;
+        var index = 0;
 
+        while (true)
+        {
+            yield return Points[index];
+            if (Points.Length == 1) continue;
+
+            if (index <= 0) direction = 1;
+
+            if (index >= Points.Length - 1) direction = -1;
+
+            index = index + direction;
+        }
+    }
 
     void OnDrawGizmos()
     {
